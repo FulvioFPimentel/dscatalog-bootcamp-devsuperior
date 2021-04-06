0f3bbc71e1c6f5ac82651ca843f8c2033f321c95
FulvioFPimentel <fulviofp@hotmail.com>
2021-04-06 01:03:39 -0300

    Basic route engine

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import './core/assets/styles/custom.scss';
 import './App.scss'
-import Navbar from './core/components/Navbar';
+import Routes from './Routes';
 
 const App = () => {
     return (
-       <Navbar/>
+       <Routes />
     );
 }
 
--- front-web/src/core/components/Navbar/index.tsx
+++ front-web/src/core/components/Navbar/index.tsx
@@ -1,29 +1,30 @@
 import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
 import './styles.scss';
 
 const Navbar = () => (
     <nav className="row bg-primary main-nav">
         <div className="col-2">
-            <a href="link" className="nav-logo-text">
+            <Link to="/" className="nav-logo-text">
                 <h4>DS Catalog</h4>
-            </a>
+            </Link>
         </div>
         <div className="col-6 offset-2">
             <ul className="main-menu">
                 <li>
-                    <a href="link" className="active">
+                    <NavLink to="/" activeClassName="active" exact>
                         HOME
-                    </a>
+                    </NavLink>
                 </li>
                 <li>
-                    <a href="link">
+                    <NavLink to="/catalog" activeClassName="active">
                         CALÁLOGO
-                    </a>
+                    </NavLink>
                 </li>
                 <li>
-                    <a href="link">
+                    <NavLink to="/admin" activeClassName="active">
                         ADMIN
-                    </a>
+                    </NavLink>
                 </li>
             </ul>
         </div>
