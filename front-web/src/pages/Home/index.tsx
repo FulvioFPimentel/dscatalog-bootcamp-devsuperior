d6fd50d1ea530bdc6898cd6b320384755dd29d2e
FulvioFPimentel <fulviofp@hotmail.com>
2021-04-07 22:11:58 -0300

    Finish home page

--- front-web/src/pages/Home/index.tsx
+++ front-web/src/pages/Home/index.tsx
@@ -1,8 +1,25 @@
 import React from 'react';
+import { ReactComponent as MainImage } from '../../core/assets/images/main-image.svg'
+import ButtonIcon from '../../core/components/ButtonIcon';
+import { Link } from 'react-router-dom';
+import "./styles.scss";
 
 const Home = () => (
-    <div>
-        <h1>Home</h1>
+    <div className="home-container">
+        <div className="row home-content">
+            <div className="col-6 home-text">
+                <h1 className="text-title">Conheça o melhor<br /> catálogo de produtos</h1>
+                <p className="text-subtitle">
+                Ajudaremos você a encontrar os melhores<br /> produtos disponíveis no mercado.
+                </p>
+                <Link to="/catalog">
+                    <ButtonIcon text="inicie agora a sua busca"/>
+                </Link>
+            </div>
+            <div className="col-6">
+                <MainImage className="Main-image"/>
+            </div>
+        </div>
     </div>
 );
 
