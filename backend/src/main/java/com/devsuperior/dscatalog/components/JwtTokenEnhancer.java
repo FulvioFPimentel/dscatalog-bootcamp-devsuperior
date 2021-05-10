b6f30bc72e62eecaae1b5b7ae8b8ae1c0c1d505b
FulvioFPimentel <fulviofp@hotmail.com>
2021-05-10 12:13:08 -0300

    Resource server

--- backend/src/main/java/com/devsuperior/dscatalog/components/JwtTokenEnhancer.java
+++ backend/src/main/java/com/devsuperior/dscatalog/components/JwtTokenEnhancer.java
@@ -5,7 +5,6 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
-import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
 import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.security.oauth2.provider.token.TokenEnhancer;
