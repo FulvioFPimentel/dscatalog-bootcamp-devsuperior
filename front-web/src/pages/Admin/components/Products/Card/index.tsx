ae023ad602d70f09566baa9527af11da13c872e1
FulvioFPimentel <fulviofp@hotmail.com>
2021-09-14 17:56:53 -0300

    Integrate product listing with backend

--- front-web/src/pages/Admin/components/Products/Card/index.tsx
+++ front-web/src/pages/Admin/components/Products/Card/index.tsx
@@ -1,28 +1,33 @@
 import ProductPrice from 'core/components/ProductPrice';
+import { Product } from 'core/types/Product';
 import React from 'react';
 import './styles.scss'
 
-const Card = () => {
+type Props = {
+    product: Product;
+}
+
+const Card = ({ product }:Props) => {
     return (
         <div>
             <h1 className="card-base product-card-admin">
                 <div className="row">
                     <div className="col-2 text-center border-right py-3">
                         <img 
-                            src="https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/4-big.jpg" 
-                            alt="Produto teste"
+                            src={product.imgUrl} 
+                            alt={product.name}
                             className="product-card-image-admin"
                         />
                     </div>
                     <div className="col-7 py-3">
                         <h3 className="product-card-name-admin">
-                            Computador i7
+                            {product.name}
                         </h3>
-                        <ProductPrice price={40.5} />
+                        <ProductPrice price={product.price} />
                         <div>
-                            <span className="badge badge-pill badge-secondary mr-2">Categoria 1</span>
-                            <span className="badge badge-pill badge-secondary mr-2">Categoria 2</span>
-                            <span className="badge badge-pill badge-secondary mr-2">Categoria 3</span>
+                            {product.categories.map(category => (
+                                <span className="badge badge-pill badge-secondary mr-2">{category.name}</span>
+                            ))}
                         </div>
                     </div>
                     <div className="col-3 pt-3 pr-5">
--- front-web/src/pages/Admin/components/Products/List/index.tsx
+++ front-web/src/pages/Admin/components/Products/List/index.tsx
@@ -1,10 +1,32 @@
-import React from 'react';
+import Pagination from 'core/components/Pagination';
+import { ProductsResponse } from 'core/types/Product';
+import { makeRequest } from 'core/utils/request';
+import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom'
 import Card from '../Card'
 
 const List = () => {
-
+    const[productsResponse, setProductResponse] = useState<ProductsResponse>();
+    const [isLoading, setIsLoading] = useState(false);
+    const [activePage, setActivePage] = useState(0);
     const history = useHistory();
+    
+    useEffect(() => {
+        const params = {
+            page: activePage,
+            linesPerPages: 4
+        }
+
+        setIsLoading(true);
+        makeRequest({ url: '/products', params})
+            .then(response => setProductResponse(response.data))
+            .finally(() => {
+                setIsLoading(false);
+            })
+
+    }, [activePage]);
+
+   
 
     const handleCreate = () => {
         history.push('/admin/products/create');
@@ -16,9 +38,15 @@ const List = () => {
                 ADICIONAR
             </button>
             <div className="admin-list-container">
-                <Card />
-                <Card />
-                <Card />
+                {productsResponse?.content.map(product => (
+                    <Card product={product} key={product.id}/>
+                ))}
+                {productsResponse && (
+                    <Pagination 
+                        totalPages={productsResponse.totalPages}
+                        activePage={activePage}
+                        onChange={page => setActivePage(page)}
+                    />)}
             </div>
         </div>
     ) 
